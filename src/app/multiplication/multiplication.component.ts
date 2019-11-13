fb5bd546b8506dd06cf9ba535f157f2f3ec5a74e
jfhr <31999724+jfhr@users.noreply.github.com>
2019-11-13 23:28:29 +0100

    resolve undeclared component

--- src/app/multiplication/multiplication.component.ts
+++ src/app/multiplication/multiplication.component.ts
@@ -2,9 +2,9 @@ import {Component} from '@angular/core';
 import {ExerciseComponent} from '../services/exercise-component';
 import {MultiplicationExercise, MultiplicationExplanationStep} from './services/multiplication-exercise-generator';
 
-@Component({
-  templateUrl: './multiplication.component.html',
-  styleUrls: ['./multiplication.component.css']
-})
+// @Component({
+//   templateUrl: './multiplication.component.html',
+//   styleUrls: ['./multiplication.component.css']
+// })
 export class MultiplicationComponent extends ExerciseComponent<MultiplicationExercise, MultiplicationExplanationStep> {
 }
