f469cfe003968eef411ba95c8c56237e1429bcf5
jfhr <me@jfhr.de>
2021-05-05 22:22:08 +0200

    Update @angular/cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,8 +41,9 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, {
-      paramsInheritanceStrategy: 'always',
-    }),
+    paramsInheritanceStrategy: 'always',
+    relativeLinkResolution: 'legacy'
+}),
     FormsModule
   ],
   providers: [
